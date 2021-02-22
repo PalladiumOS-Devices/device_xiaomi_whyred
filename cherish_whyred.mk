b3c7e5c6196a809973640442a429a79b85577b7b
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2021-02-22 18:18:39 +0530

    Whyred : Welcome CherishOS !

--- cherish_whyred.mk
+++ cherish_whyred.mk
@@ -19,14 +19,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common CherishOS stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-# PixelExperience Properties
+# Cherish Properties
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+CHERISH_BUILD_TYPE := Official
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -34,10 +35,11 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Inherit from custom vendor.
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
+# Device Specfic
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := cherish_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
