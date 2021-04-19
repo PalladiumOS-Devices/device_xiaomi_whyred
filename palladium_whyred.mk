54c6c821a276c85580f8c3fb61a26458679103f0
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2021-04-19 22:24:31 +0530

    whyred : Update To April's Redfin PATCH !

--- palladium_whyred.mk
+++ palladium_whyred.mk
@@ -57,6 +57,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
+    TARGET_DEVICE="whyred" \
+    PRODUCT_NAME="whyred" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
 
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
+#Build FP to be picked by both system and vendor
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
