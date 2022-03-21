3bd5aac6e7812e99bbecc0d68555b0aa1c758917
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2022-03-21 11:03:42 -0400

    Revert "whyred : Add TapTap | Best Alternative of QuickTap in Pixel."
    This reverts commit b1cd855c9a120b39a3fb87a7ce64c68a5a7f339d.

--- device.mk
+++ device.mk
@@ -59,9 +59,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# TapTap
-PRODUCT_PACKAGES += \
-    TapTap
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
